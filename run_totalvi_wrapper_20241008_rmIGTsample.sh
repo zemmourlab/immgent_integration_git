ace613e3a6a9fba0a9b0dddfd138c2a458932a08
dzemmour <zemmmour@uchicago.edu>
2024-10-08 21:41:19 -0500

    debug

--- run_totalvi_wrapper_20241008_rmIGTsample.sh
+++ run_totalvi_wrapper_20241008_rmIGTsample.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL ; 
 #SBATCH --mail-user=zemmour@rcc.uchicago.edu   # Email to which notifications will be sent
 
-#run as: sbatch $SCRIPT_DIR/run_totalvi_wrapper_20241006.sh
+#run as: sbatch $SCRIPT_DIR/run_totalvi_wrapper_20241008_rmIGTsample.sh
 
 module load python
 
