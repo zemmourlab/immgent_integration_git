af3342efeb0b4840f0c36d32d50111e7cf24223d
dzemmour <zemmmour@uchicago.edu>
2024-10-14 16:28:49 -0500

    debug

--- run_totalvi_wrapper_20241014_miniverse_rmIGTsample.sh
+++ run_totalvi_wrapper_20241014_miniverse_rmIGTsample.sh
@@ -2,28 +2,26 @@
 #SBATCH -A pi-zemmour ##SBATCH -q jfkfloor2 --exclusive 
 #SBATCH --partition=beagle3 
 #SBATCH --gres=gpu:1 
-#SBATCH --mem=128GB #184GB
-#SBATCH -J totalvi              
-#SBATCH -o totalviv2_20241008.log
-#SBATCH -t 36:00:00              ##SBATCH --mem=8G
+#SBATCH --mem=64GB #184GB
+#SBATCH -J miniversetotalvi              
+#SBATCH -o totalviv2_20241014_miniverse.log
+#SBATCH -t 24:00:00              ##SBATCH --mem=8G
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL ; 
 #SBATCH --mail-user=zemmour@rcc.uchicago.edu   # Email to which notifications will be sent
 
-#run as: sbatch $SCRIPT_DIR/run_totalvi_wrapper_20241008_rmIGTsample.sh
+#run as: sbatch $SCRIPT_DIR/run_totalvi_wrapper_20241014_miniverse_rmIGTsample.sh
 
 module load python
 
 source activate /project/zemmour/david/envs/scvi120_20241008
 
 SCRIPT_DIR=/project/jfkfloor2/zemmourlab/david/immgent/immgent_integration_git
-working_dir=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/totalvi_20241008_rmIGTsample
-path_to_mudata=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/export_data/igt1_96_20241006.h5mu
-prefix=totalvi_20241008_rmIGTsample
+working_dir=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/
+path_to_mudata=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/export_data/igt1_96_20241014_miniverse.h5mu
+prefix=totalvi_20241014_miniverse_rmIGTsample
 batchkey=IGT
 categorical_covariate_keys=IGTHT
 corrected_counts=False
 denoised_data=False
-
 cd $working_dir
-
 python $SCRIPT_DIR/run_totalvi_v2.py --working_dir=$working_dir --path_to_mudata=$path_to_mudata --prefix=$prefix --batchkey=$batchkey --categorical_covariate_keys=$categorical_covariate_keys --corrected_counts=$corrected_counts --denoised_data=$denoised_data
