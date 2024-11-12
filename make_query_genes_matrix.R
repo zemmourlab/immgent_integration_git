73853986be5bf353adfcf4e7762f52148c4e1244
Casey <Odhran_Casey@hms.harvard.edu>
2024-11-12 17:04:00 -0500

    R script for generating query genes matrix and extracting metadata from seurat object (found via accession instructions on the paper). Resulting matrix and metadata used as input to train_query_model_unconventional_v2.py

---
+++ make_query_genes_matrix.R
@@ -0,0 +1,46 @@
+### Pre-process paper data ###
+
+library(dplyr)
+library(Seurat)
+library(SeuratObject)
+library(MASS)
+library(readxl)
+library(DropletUtils)
+
+so_path = "N:/CBDM_Lab/Odhran/scVerse/ImmgenT_Workshop/unconventional/Tgd/"
+so_name = "gd_holygrail_filtered_final.rds"
+batche_of_interest = "batch1_oct"
+
+# for seurat object
+so <- readRDS(paste0(so_path, so_name))
+View(so@meta.data)
+so <- so[,so$batch ==batch_of_interest]
+query_df = as.data.frame(so@assays$RNA@counts)
+genes_query = rownames(so)
+cells_query = colnames(so)
+
+# make genes matrix
+query_df = query_df[rownames(query_df) %in% genes,]
+gene_diff = length(genes) - length(rownames(query_df))
+missing_genes = setdiff(genes, rownames(query_df))
+
+zero_rows <- as.data.frame(matrix(0, nrow = gene_diff, ncol = length(cells_query)))
+colnames(zero_rows) <- colnames(query_df)
+rownames(zero_rows) <- missing_genes
+query_df_all = rbind(query_df, zero_rows)
+
+query_df_reorder = query_df_all[genes,]
+
+Query_matrix_finished = as(query_df_reorder, "sparseMatrix")
+
+meta = so@meta.data
+write.csv2(meta, paste0(so_path, "cell_metadata.csv"))
+
+write10xCounts(
+  paste0(so_path, "matrix"),
+  Query_matrix_finished,
+  barcodes = cells_query,
+  gene.id = genes,
+  gene.symbol = genes,
+  overwrite = TRUE
+)
