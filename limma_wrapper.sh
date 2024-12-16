46a8cbb4337aabb3c808226706b1349866a738e0
dzemmour <zemmmour@uchicago.edu>
2024-12-16 15:50:53 -0600

    limma

--- limma_wrapper.sh
+++ limma_wrapper.sh
@@ -39,7 +39,11 @@ fit_file_name=igt1_96_CD4_20241113_sampled_fit.Rds
 
 path_to_fit_object=$output_dir/$fit_file_name
 prefix_file_name=in_Resting
-Rscript $SCRIPT_DIR/limma_contrasts_inResting.R $path_to_seurat_object $path_to_tmm_object $path_to_fit_object $output_dir $prefix_file_name
+# Rscript $SCRIPT_DIR/limma_contrasts_inResting.R $path_to_seurat_object $path_to_tmm_object $path_to_fit_object $output_dir $prefix_file_name
+
+path_to_fit_object=$output_dir/$fit_file_name
+prefix_file_name=in_Activated
+Rscript $SCRIPT_DIR/limma_contrasts_inActivated.R $path_to_seurat_object $path_to_tmm_object $path_to_fit_object $output_dir $prefix_file_name
 
 
 
