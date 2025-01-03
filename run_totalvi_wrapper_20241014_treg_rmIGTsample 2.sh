e825f77afdd2c3c0f7129ce1261c4ef66116b6a3
dzemmour <zemmmour@uchicago.edu>
2025-01-03 13:15:52 -0600

    re tracking files

---
+++ run_totalvi_wrapper_20241014_treg_rmIGTsample 2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -A pi-zemmour ##SBATCH -q jfkfloor2 --exclusive 
+#SBATCH --partition=beagle3 
+#SBATCH --gres=gpu:1 
+#SBATCH --mem=64GB #184GB
+#SBATCH -J totalvitreg              
+#SBATCH -o totalviv2_20241014_treg.log
+#SBATCH -t 24:00:00              ##SBATCH --mem=8G
+#SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL ; 
+#SBATCH --mail-user=zemmour@rcc.uchicago.edu   # Email to which notifications will be sent
+
+#run as: sbatch $SCRIPT_DIR/run_totalvi_wrapper_20241014_treg_rmIGTsample.sh
+
+module load python
+
+source activate /project/zemmour/david/envs/scvi120_20241008
+
+SCRIPT_DIR=/project/jfkfloor2/zemmourlab/david/immgent/immgent_integration_git
+working_dir=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/
+path_to_mudata=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/export_data/igt1_96_20241014_Treg.h5mu
+prefix=totalvi_20241014_Treg_rmIGTsample
+batchkey=IGT
+categorical_covariate_keys=IGTHT
+corrected_counts=False
+denoised_data=False
+cd $working_dir
+python $SCRIPT_DIR/run_totalvi_v2.py --working_dir=$working_dir --path_to_mudata=$path_to_mudata --prefix=$prefix --batchkey=$batchkey --categorical_covariate_keys=$categorical_covariate_keys --corrected_counts=$corrected_counts --denoised_data=$denoised_data
