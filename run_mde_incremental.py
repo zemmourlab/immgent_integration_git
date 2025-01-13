5b28d74544b086dbdb1bcdec2c341349e776b0cd
dzemmour <zemmmour@uchicago.edu>
2025-01-12 23:07:37 -0600

    debug

--- run_mde_incremental.py
+++ run_mde_incremental.py
@@ -77,61 +77,65 @@ print("Reading integrated ref+query latent space")
 totalvi_integrated = pd.read_csv(totalvi_integrated_file, index_col = 0, header = 0)
 
 print("anchor MDE")
-#test dataset
-# mde_ref_embedding_test = mde_ref_embedding[:5000]
-# rownames_tensor_test = rownames_tensor[:5000]
-# common_indices = totalvi_integrated.index.intersection(mde_ref_embedding_test.index)
-# other_indices = totalvi_integrated.index.difference(mde_ref_embedding_test.index)
-# random_sample_indices = np.random.choice(other_indices, size=1000, replace=False)
-# combined_indices = common_indices.union(random_sample_indices)
-# totalvi_integrated_test = totalvi_integrated.loc[combined_indices]
+##test dataset to troubleshoot
+## mde_ref_embedding_test = mde_ref_embedding[:5000]
+## rownames_tensor_test = rownames_tensor[:5000]
+## common_indices = totalvi_integrated.index.intersection(mde_ref_embedding_test.index)
+## other_indices = totalvi_integrated.index.difference(mde_ref_embedding_test.index)
+## random_sample_indices = np.random.choice(other_indices, size=1000, replace=False)
+## combined_indices = common_indices.union(random_sample_indices)
+## totalvi_integrated_test = totalvi_integrated.loc[combined_indices]
 
-#match mde_ref_embedding with index in totalvi_integrated
-mde_ref_embedding = pd.read_csv(mde_ref_file, index_col = 0, header = 0)
-mde_ref_embedding = mde_ref_embedding.loc[mde_ref_embedding.index.intersection(totalvi_integrated.index)] #subset cells present in the integrated space other wise mismatch between anchors and values in pymde.Anchored
-rownames = mde_ref_embedding.index.tolist()  # Convert index to list
-index_positions = [totalvi_integrated.index.get_loc(item) for item in rownames if item in totalvi_integrated.index]
-len(index_positions)
-len(mde_ref_embedding.index)
-rownames_tensor = torch.tensor(index_positions, device='cuda:0')
+# match mde_ref_embedding with index in totalvi_integrated
+# mde_ref_embedding = pd.read_csv(mde_ref_file, index_col = 0, header = 0)
+# mde_ref_embedding = mde_ref_embedding.loc[mde_ref_embedding.index.intersection(totalvi_integrated.index)] #subset cells present in the integrated space other wise mismatch between anchors and values in pymde.Anchored
+# rownames = mde_ref_embedding.index.tolist()  # Convert index to list
+# index_positions = [totalvi_integrated.index.get_loc(item) for item in rownames if item in totalvi_integrated.index]
+# len(index_positions)
+# len(mde_ref_embedding.index)
+# rownames_tensor = torch.tensor(index_positions, device='cuda:0')
 
-print("pymde.Anchored")
-anchor_constraint = pymde.Anchored(
-    anchors=rownames_tensor,
-    values=torch.tensor(mde_ref_embedding.values, dtype=torch.float32,device='cuda:0'), #device='cpu' 'cuda:0'
-)
-
-print("pymde.preserve_neighbors")
-incremental_mde = pymde.preserve_neighbors(
-    torch.tensor(totalvi_integrated.values, dtype=torch.float32,device='cuda:0'), #device='cpu'
-    embedding_dim=2,
-    constraint=anchor_constraint,
-    repulsive_fraction=0.7,
-    n_neighbors = 15,
-    verbose=True,
-    device='cuda:0'
-)
-
+# print("pymde.Anchored")
+# anchor_constraint = pymde.Anchored(
+#     anchors=rownames_tensor,
+#     values=torch.tensor(mde_ref_embedding.values, dtype=torch.float32,device='cuda:0'), #device='cpu' 'cuda:0'
+# )
+# 
+# print("pymde.preserve_neighbors")
 # incremental_mde = pymde.preserve_neighbors(
-#     torch.tensor(totalvi_integrated.values, dtype=torch.float32), #device='cpu'
+#     torch.tensor(totalvi_integrated.values, dtype=torch.float32,device='cuda:0'), #device='cpu'
+#     embedding_dim=2,
 #     constraint=anchor_constraint,
-#     init='random',
-#     verbose=True #,device = 'cpu'
-#     )
+#     repulsive_fraction=0.7,
+#     n_neighbors = 15,
+#     verbose=True,
+#     device='cuda:0'
+# )
 
-print("incremental_mde.embed")
-incremental_mde.embed(eps=1e-6, verbose=True)
+#if needed to run without anchoring
+## incremental_mde = pymde.preserve_neighbors(
+##     torch.tensor(totalvi_integrated.values, dtype=torch.float32), #device='cpu'
+##     constraint=anchor_constraint,
+##     init='random',
+##     verbose=True #,device = 'cpu'
+##     )
 
-pymde.plot(mde_ref_embedding.values)
-fig = plt.gcf() 
-fig.savefig(prefix+"/mde_incremental_original.png")
-pymde.plot(incremental_mde.X)
-fig = plt.gcf() 
-fig.savefig(prefix+"/mde_incremental.png")
+# print("incremental_mde.embed")
+# incremental_mde.embed(eps=1e-6, verbose=True)
+# 
+# pymde.plot(mde_ref_embedding.values)
+# fig = plt.gcf() 
+# fig.savefig(prefix+"/mde_incremental_original.png")
+# pymde.plot(incremental_mde.X)
+# fig = plt.gcf() 
+# fig.savefig(prefix+"/mde_incremental.png")
+# 
+# model_pkl_file = prefix+'/mde_incremental_model.pkl' 
+# with open(model_pkl_file, 'wb') as file:  
+#     pickle.dump(incremental_mde, file)
+    
+with open(model_pkl_file, 'rb') as file:
+    incremental_mde = pickle.load(file)
 
-model_pkl_file = prefix+'/mde_incremental_model.pkl' 
-with open(model_pkl_file, 'wb') as file:  
-    pickle.dump(incremental_mde, file)
-
-incremental_mde_df = pd.DataFrame(incremental_mde.X, index = totalvi_integrated.index)
+incremental_mde_df = pd.DataFrame(incremental_mde.X.cpu().numpy(), index = totalvi_integrated.index)
 incremental_mde_df.to_csv(prefix+'/mde_incremental.csv' , index=True)
