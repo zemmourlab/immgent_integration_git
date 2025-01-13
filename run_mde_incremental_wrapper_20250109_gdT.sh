234d35faf039b0e9ea58035fe9aee093a63a946d
dzemmour <zemmmour@uchicago.edu>
2025-01-12 21:10:08 -0600

    debug

--- run_mde_incremental_wrapper_20250109_gdT.sh
+++ run_mde_incremental_wrapper_20250109_gdT.sh
@@ -19,7 +19,7 @@ source activate /project/zemmour/david/envs/scvi120_20241008
 SCRIPT_DIR=/project/jfkfloor2/zemmourlab/david/immgent/immgent_integration_git
 working_dir=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/gdT
 prefix=20250109_gdT
-mde_ref_file=totalvi_20241201_gdT_rmIGTsample/mde2_nooutliers.csv
+mde_ref_file=totalvi_20241201_gdT_rmIGTsample/mde2.csv
 totalvi_integrated_file=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/totalvi_20241008_rmIGTsample/latent_gdT.csv
 cd $working_dir
 python $SCRIPT_DIR/run_mde_incremental.py --working_dir=$working_dir --prefix=$prefix --mde_ref_file=$mde_ref_file --totalvi_integrated_file=$totalvi_integrated_file
