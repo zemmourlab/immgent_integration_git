1e14bcc1874b3153fbdf841ac01679142b193578
dzemmour <zemmmour@uchicago.edu>
2025-01-21 20:09:48 -0600

    debug

--- limma_wrapper_20250109_nonconv.sh
+++ limma_wrapper_20250109_nonconv.sh
@@ -27,17 +27,17 @@ echo "Sampling seurat object"
 path_to_seurat_object=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/nonconv/igt1_96_nonconv_20250109.Rds
 output_dir=DGE_limma/20250109
 so_file_name=igt1_96_nonconv_20250109_sampled_so.Rds
-Rscript $SCRIPT_DIR/limma_sample_seuratobject_20241217.R $path_to_seurat_object $output_dir $so_file_name
+# Rscript $SCRIPT_DIR/limma_sample_seuratobject_20241217.R $path_to_seurat_object $output_dir $so_file_name
 
 echo "Making tmm file"
 path_to_seurat_object=$output_dir/$so_file_name
 tmm_file_name=igt1_96_nonconv_20250109_tmm.Rds
-Rscript $SCRIPT_DIR/limma_make_tmm_20241217.R $path_to_seurat_object $output_dir $tmm_file_name
+# Rscript $SCRIPT_DIR/limma_make_tmm_20241217.R $path_to_seurat_object $output_dir $tmm_file_name
 
 echo "Fitting"
 path_to_tmm_object=$output_dir/$tmm_file_name
 fit_file_name=igt1_96_nonconv_20250109_fit.Rds
-Rscript $SCRIPT_DIR/limma_fit_20241217_level2.IGTHT.R $path_to_seurat_object $path_to_tmm_object $output_dir $fit_file_name
+# Rscript $SCRIPT_DIR/limma_fit_20241217_level2.IGTHT.R $path_to_seurat_object $path_to_tmm_object $output_dir $fit_file_name
 
 echo "OneVsAll"
 path_to_fit_object=$output_dir/$fit_file_name
