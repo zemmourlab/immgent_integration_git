419b3431164781ab7960a0114f2842e6762cf78f
dzemmour <zemmmour@uchicago.edu>
2025-01-24 10:27:35 -0600

    update

--- limma_wrapper_20250109_All.sh
+++ limma_wrapper_20250109_All.sh
@@ -27,12 +27,12 @@ echo "Sampling seurat object"
 path_to_seurat_object=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/igt1_96_withtotalvi20250109_clean.Rds
 output_dir=DGE_limma/20250109
 so_file_name=igt1_96_withtotalvi20250109_clean_sampled.Rds
-Rscript $SCRIPT_DIR/limma_sample_seuratobject_20241217.R $path_to_seurat_object $output_dir $so_file_name
+# Rscript $SCRIPT_DIR/limma_sample_seuratobject_20241217.R $path_to_seurat_object $output_dir $so_file_name
 
 echo "Making tmm file"
 path_to_seurat_object=$output_dir/$so_file_name
 tmm_file_name=igt1_96_20250109_tmm.Rds
-Rscript $SCRIPT_DIR/limma_make_tmm_20241217.R $path_to_seurat_object $output_dir $tmm_file_name
+# Rscript $SCRIPT_DIR/limma_make_tmm_20241217.R $path_to_seurat_object $output_dir $tmm_file_name
 
 echo "Fitting"
 path_to_tmm_object=$output_dir/$tmm_file_name
