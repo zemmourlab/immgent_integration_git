427b02cb450b588bbc9e1e17a90a2883ebac58aa
dzemmour <zemmmour@uchicago.edu>
2025-02-03 10:00:20 -0600

    topic update

---
+++ topic_wrapper_template.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -A pi-zemmour ##SBATCH -q jfkfloor2 --exclusive 
+#SBATCH --partition=zemmour-hm 
+#SBATCH --nodes=1 
+#SBATCH --mem=0
+#SBATCH -J topic_%j            
+#SBATCH -o topic_%j.log
+#SBATCH -t 7-00:00:00              ##SBATCH --mem=8G
+#SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL ; 
+#SBATCH --mail-user=zemmour@rcc.uchicago.edu   # Email to which notifications will be sent
+
+#run as: sbatch $SCRIPT_DIR/topic_wrapper.sh
+
+module load python/anaconda-2022.05 
+source $(conda info --base)/etc/profile.d/conda.sh
+module load hdf5/1.12.0 #for BPCells but also others
+module load openblas/0.3.13
+conda activate /project/zemmour/david/envs/scvi_20240315
+module load openblas/0.3.13
+
+SCRIPT_DIR=/project/jfkfloor2/zemmourlab/david/immgent/immgent_integration_git
+path_to_wd=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/
+
+cd $path_to_wd
+
+path_to_seurat_object=/project/zemmour/david/ImmgenT/analysis/data_integration/IGT1_96/DGE_limma/20250109/igt1_96_withtotalvi20250109_clean_sampled.Rds
+output_dir=topic/flashier20250203
+
+Rscript $SCRIPT_DIR/topic_flashier_20250203.R $path_to_seurat_object $output_dir
+
